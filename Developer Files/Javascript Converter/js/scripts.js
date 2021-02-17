d7d22423e2dca9e646728716f7c2aa1e4217f236
Brendan <blwong@outlook.com.au>
2021-02-17 16:31:09 +1000

    Initial support for SP Lite Hanover EG3
    Code refactoring and initial support for Studio Polygon Lite with Hanover EG3

--- Developer Files/Javascript Converter/js/scripts.js
+++ Developer Files/Javascript Converter/js/scripts.js
@@ -1,51 +1,71 @@
 window.addEventListener("load", start);
 
-let customRouteFont = false
-
 function start() {
     document.getElementById("build").addEventListener("click", convert)
     document.getElementById("copy").addEventListener("click", copyToClipboard);
-}
-
-function copyToClipboard(){
-    document.getElementById("output").select();
-    document.execCommand("copy");
 }
 
 function convert() {
     let contents = document.getElementById("input").value;
     let title = document.getElementById("title").value;
     let version = document.getElementById("version").value;
+    let hanover = document.getElementById("hanover").checked;
+    let customFont = document.getElementById("customFonts").checked;
     //contents = contents.split("\t");
     contents = contents.split("\n");
     for (let i = 0; i < contents.length; i++) {
         contents[i] = contents[i].split("\t");
     }
-    output(contents, version, title);
+    output(contents, version, title, hanover, customFont);
 }
 
+function output(contents, version, title, hanover, customFont) {
+    let [terminus, serviceTrip, routes] = generateDestoData(contents, version, title, hanover, customFont);
 
-function output(desto, version, title) {
+    let output = (hanover) ?
+        generateHeader("Studio Polygon EG3 Controller with Scrolling Mod",title,serviceTrip)
+        : generateHeader("Citaro RHD with Controller Mod",title,serviceTrip);
+
+    output += terminus;
+    output += (customFont && !hanover) ?
+        "	14994	Custom number	BT_Numbers_Old					\r\n"
+        + "	14998	Custom number	BT_Numbers_Old					\r\n"
+        : "";
+    output += "[end]\r\n\r\n[addbusstop_list]\r\n[end]\r\n\r\n";
+    output += routes;
+    document.getElementById("output").value = output;
+}
+
+function generateDestoData(desto, version, title, hanover, customFont) {
     let terminus = "";
     let routes = "";
     let serviceTrip = "";
-
     for (let i = 0; i < desto.length; i++) {
         let prepayScreen = false;
         let twoScreens = false
         let flip = false
+        let split = false;
+        let IBISDesto = "";
+        let rearSmallTop = ""
+        let rearSmallBottom = ""
+
+        if (typeof desto[i][18] != "undefined") {
+            const splitRear = desto[i][18].split('|');
+            rearSmallTop = splitRear[0]
+            rearSmallBottom = splitRear[1]
+        }
+
 
         // Skip if line empty or no Destination set
         if (desto[i].length === 1 || desto[i][4] == "") {
             continue;
         }
-        desto[i][1] = desto[i][1].padStart(4,0).insert(3,0);
-
+        IBISDesto = desto[i][1].padStart(4, "0").insert(3, 0);
         // Skip line if doesn't match selected version.
         if (!desto[i][2].includes(version) && desto[i][2] != "") {
             continue;
         }
-        
+
         // Set Service Trip
         if (desto[i][12].includes("%")) {
             serviceTrip = desto[i][4];
@@ -58,7 +78,7 @@ function output(desto, version, title) {
         }
 
         if (desto[i][12].includes("^")) {
-            prepayScreen = true;
+            flip = true;
             desto[i][12] = desto[i][12].replace("^", "");
         }
 
@@ -68,24 +88,41 @@ function output(desto, version, title) {
         }
 
         if (desto[i][12].includes("*IT")) {
-            const split = desto[i][7].split('|');
-            terminus += allExit(desto[i][3]); // Exit
-            terminus += "\t" + i; // Ziel
-            terminus += "\t" + desto[i][4]; // Destination
-            terminus += "\t" + IBISOutput (1, version, desto[i]); // Old IBIS
-            terminus += "\t" + split[0] + desto[i][13]; // Top Line
-            terminus += "\t" + split[1] + desto[i][14]; // Bottom Line
-            if (desto[i][0] !== "") {
-                terminus += "*L[" + desto[i][0] + "]";
-            }
-            terminus += "\t" + desto[i][6] + desto[i][12]; // Half Split Line
-            terminus += footer (version, desto[i]);
+            split = true;
+        }
+        if (split) {
+            const splitParts = desto[i][7].split('|');
+            if (hanover) terminus += outputHanoverDestoLine(
+                desto[i][3],
+                generateHanoverScroll(desto[i][1], 0, 0, desto[i][0]),
+                desto[i][4],
+                IBISOutput(2, version, desto[i]),
+                desto[i][6],
+                splitParts[0] + " " + splitParts[1],
+                desto[i][6],
+                (desto[i][12].includes("*N")) ? " " : desto[i][0],
+                "",
+                rearSmallTop,
+                rearSmallBottom,
+                desto[i][6],
+                splitParts[0] + " " + splitParts[1]
+            )
+            else terminus += outputKrugerDestoLine(
+                desto[i][3],
+                i,
+                desto[i][4],
+                IBISOutput(1, version, desto[i]),
+                splitParts[0] + desto[i][13],
+                splitParts[1] + desto[i][14] + manageRouteNumber(desto[i], customFont),
+                desto[i][6] + desto[i][12],
+                IBISOutput(2, version, desto[i])
+            )
         } else {
             let scrolling = 0;
             let current = 0;
             if (desto[i][8] !== "") {
                 if (desto[i][9] !== ""
-                    && !desto[i][14].includes("!")) {
+                    && !twoScreens) {
                     if (desto[i][10] !== "") {
                         scrolling = 3;
                     } else if (desto[i][11] !== "") {
@@ -97,106 +134,102 @@ function output(desto, version, title) {
             }
 
             while (current <= scrolling) {
-                terminus += allExit(desto[i][3]); // Exit
-                terminus += "\t";
-                terminus += (current * 1000) + i; // Ziel
-                terminus += "\t" + desto[i][4]; // Destination
-                terminus += "\t" + IBISOutput (1, version, desto[i]); // Old IBIS
-                if (current === 1 && twoScreens) {
-                    terminus += "\t" + desto[i][8] + desto[i][14]; // Top Line
-                    terminus += forceRoute(desto[i][0]);
-                    terminus += "\t" + desto[i][9] + desto[i][15]; // Bottom Line
-                } else if (flip) {
-                    terminus += "\t" + desto[i][7 + current] + desto[i][13 + current]; // Top Line
-                    terminus += forceRoute(desto[i][0]);
-                    terminus += "\t" + desto[i][6] + desto[i][12]; // Bottom Line
-                } else {
-                    terminus += "\t" + desto[i][6] + desto[i][12]; // Top Line
-                    terminus += forceRoute(desto[i][0]);
-                    terminus += "\t" + desto[i][7 + current] + desto[i][13 + current]; // Bottom Line
-                }
+                var top = "";
+                var bottom = "";
+                if (hanover) {
+                    if (current === 1 && twoScreens) {
+                        top = desto[i][8]; // Top Line
+                        bottom = desto[i][9]; // Bottom Line
+                    } else if (flip) {
+                        top = desto[i][7 + current]; // Top Line
+                        bottom = desto[i][6]; // Bottom Line
+                    } else {
+                        top = desto[i][6]; // Top Line
+                        bottom = desto[i][7 + current]; // Bottom Line
+                    }
 
-                if (!desto[i][12].includes("*N") && desto[i][0] == "" && customRouteFont) {
-                    terminus += "*CN";
+                    terminus += outputHanoverDestoLine(
+                        desto[i][3],
+                        generateHanoverScroll(desto[i][1], current, scrolling, desto[i][0]),
+                        desto[i][4],
+                        IBISOutput(2, version, desto[i]),
+                        top,
+                        bottom,
+                        top,
+                        (desto[i][12].includes("*N")) ? " " : desto[i][0],
+                        "",
+                        rearSmallTop,
+                        rearSmallBottom,
+                        top,
+                        bottom
+                    )
+                } else {
+                    if (current === 1 && twoScreens) {
+                        top = desto[i][8] + desto[i][14]; // Top Line
+                        bottom = desto[i][9] + desto[i][15]; // Bottom Line
+                    } else if (flip) {
+                        top = desto[i][7 + current] + desto[i][13 + current]; // Top Line
+                        bottom = desto[i][6] + desto[i][12]; // Bottom Line
+                    } else {
+                        top = desto[i][6] + desto[i][12]; // Top Line
+                        bottom = desto[i][7 + current] + desto[i][13 + current]; // Bottom Line
+                    }
+                    terminus += outputKrugerDestoLine(
+                        desto[i][3],
+                        (current * 1000) + i,
+                        desto[i][4],
+                        IBISOutput(1, version, desto[i]),
+                        top,
+                        bottom + manageRouteNumber(desto[i], customFont),
+                        "",
+                        IBISOutput(2, version, desto[i])
+                    )
                 }
-                terminus += footer (version, desto[i]);
-                
                 current++;
             }
             // Prepay Only
-            if (prepayScreen) {
-                terminus += allExit(desto[i][3]); // Exit
-                terminus += "\t";
-                terminus += (current * 1000) + i; // Ziel
-                terminus += "\t" + desto[i][4]; // Destination
-                terminus += "\t" + IBISOutput (1, version, desto[i]); // Old IBIS
-                terminus += "\tPREPAY ONLY*I"; // Top Line
-                terminus += forceRoute(desto[i][0]);
-                terminus += "\t"; // Bottom Line
-                terminus += footer (version, desto[i]);
+            if (prepayScreen && !hanover) {
+                terminus += outputKrugerDestoLine(
+                    desto[i][3],
+                    (current * 1000) + i,
+                    desto[i][4],
+                    IBISOutput(1, version, desto[i]),
+                    terminus += "\tPREPAY ONLY*I",
+                    manageRouteNumber(desto[i], customFont),
+                    "",
+                    IBISOutput(2, version, desto[i])
+                )
             }
         }
-        
-        routes += "[infosystem_trip]\r\n"; //opening tag
-        routes += desto[i][1] + "\r\n"; //desto code
-        routes += ((desto[i][0] == "") ? desto[i][5] : desto[i][5] + " " + desto[i][0]) + "\r\n"; // Name
-        routes += i + "\r\n";
-        routes += (desto[i][0]).replace(/\D/g, "");
-        routes += "\r\n\r\n[infosystem_busstop_list]\r\n0\r\n\r\n";
+        routes += (hanover) ? "" : generateInfoSystemTrips(desto[i], IBISDesto, i);
     }
-    let output = "\tAutogenerated File for use with the Australian Style Desto Controller\r\n\r\n";
+    return [terminus, serviceTrip, routes];
+}
+
+function generateHeader(version,title,serviceTrip) {
+    let output = "\tAutogenerated File made for QFA1213's Bounded Desto Controller Mod.\r\n"
+    output += "This HOF is designed for " + version + " only.\r\n\r\n";
+    output += "Generated: " +  Math.floor(Date.now() / 1000) + "\r\n\r\n";
     output += "[name]\r\n" + title + "\r\n\r\n";
     output += "[servicetrip]\r\n" + serviceTrip + "\r\n\r\n";
     output += "[global_strings]\r\n0\r\n\r\n";
-    output += "stringcount_terminus\r\n5\r\n\r\n";
-    output += "stringcount_busstop\r\n0\r\n\r\n";
+    output += "stringcount_terminus\r\n26\r\n\r\n";
+    output += "stringcount_busstop\r\n9\r\n\r\n";
     output += "[addterminus_list]\r\n";
-    output += terminus;
-    if (customRouteFont) {
-        output += "	14994	Custom number	BT_Numbers_Old					\r\n";
-        output += "	14998	Custom number	BT_Numbers_Old					\r\n";
-    }
-    output += "[end]\r\n\r\n[addbusstop_list]\r\n[end]\r\n\r\n";
-    output += routes;
-    document.getElementById("output").value = output;
+    return output;
 }
 
-function IBISOutput (IBISversion, Depot, Desto) {
-    var output = "";
-    const NSWDepots = ["N", "V", "T"];
-    if (NSWDepots.includes(Depot)) {
-        output = (Desto[0] == "") ? Desto[5] : Desto[0] + " " + Desto[5];
-        // Destination / Number + Destination
-    } else {
-        output = (Desto[0] == "") ? Desto[5] : Desto[5] + " " + Desto[0];
-        // Destination / Destination + Number
-    }
-    
-    switch (IBISversion) {
-        case 1:
-            return output.toUpperCase();
-        case 2:
-        default:
-            return output;
-    }
+function generateInfoSystemTrips(routeRowArray, IBISDesto, i){
+    let routes = "[infosystem_trip]\r\n"; //opening tag
+    routes += IBISDesto + "\r\n"; //desto code
+    routes += ((routeRowArray[0] == "") ? routeRowArray[5] : routeRowArray[5] + " " + routeRowArray[0]) + "\r\n"; // Name
+    routes += i + "\r\n";
+    routes += (routeRowArray[0]).replace(/\D/g, "");
+    routes += "\r\n\r\n[infosystem_busstop_list]\r\n0\r\n\r\n";
+    return routes;
 }
 
-function forceRoute(RouteNumber) {
-    return (RouteNumber !== "") ? ("*L[" + RouteNumber + "]") : "";
-}
 
-function allExit(value) {
-    return (value !== "") ? "{ALLEX}" : "";
-}
-
-function footer (version, desto) {
-    let terminus = "";
-    terminus += "\t"; // Half Split Line
-    terminus += "\t"; // Roller Img
-    terminus += "\t" + IBISOutput (2, version, desto); // IBIS 2
-    terminus += "\r\n";
-    return terminus;
-}
 
 //https://stackoverflow.com/a/9160869
 String.prototype.insert = function(index, string) {
---
+++ Developer Files/Javascript Converter/js/tools.js
@@ -0,0 +1,78 @@
+function allExit(value) {
+    return (value !== "") ? "{ALLEX}" : "";
+}
+
+function manageRouteNumber(desto, customFont){
+    if (desto[0] !== "") {
+        return "*L[" + desto[0] + "]"
+    } else if (!desto[12].includes("*N") && customFont) {
+        return "*CN";
+    } else {
+        return ""
+    }
+}
+
+function generateHanoverScroll(code, current, scrolling = 0, route) {
+    let version = 2
+    switch (version) {
+        case 0:
+            return (scrolling === 0) ? code.padStart(4, "0") : (current + 1) * 10000 + parseInt(code); //V1
+        case 1:
+            return (scrolling === 0) ? code.padStart(4, "0") : (current * 10000 + parseInt(code)).toString(); //V2
+        case 2:
+        default:
+            route = route.replace(/\D/, "");
+            if (code.indexOf(route) !== -1 && route !== "") {
+                let codeArray = code.match(/^[A-Za-z]?(\w{1,3})(\w)$/); // Invert
+                code = codeArray[2] * 1000 + parseInt(codeArray[1]);
+            }
+            //return (scrolling === 0) ? "1" + code.toString().padStart(4, "0") :  + ((current + 1) * 10000 + code).toString();
+            return (scrolling === 0) ? code.toString().padStart(4, "0") :  + ((current + 1) * 10000 + code).toString();
+    }
+}
+
+function outputKrugerDestoLine(Allex = "",Code = "",Destination = "",IBIS1 = "",
+                               Top = "",Bottom = "",Third = "",IBIS2 = "") {
+    return allExit(Allex) + "\t" +
+        Code + "\t" +
+        Destination + "\t" +
+        IBIS1 + "\t" +
+        Top + "\t" +
+        Bottom + "\t" +
+        Third + "\t" +
+        "\t\t" +
+        IBIS2 +
+        "\r\n";
+}
+
+function IBISOutput (IBISversion, Depot, Desto) {
+    let output = Desto[5] + " (" + Desto[1] + ")";
+    return (IBISversion === 1) ? output.toUpperCase() : output;
+}
+
+function outputHanoverDestoLine(Allex = "",Code = "",Destination = "",IBIS1 = "",
+                                Top = "",Bottom = "",Side = "",RouteNum = "",
+                                ImgPath = "", RearSmallTop = "", RearSmallBottom = "",
+                                RearBigTop = "", RearBigBottom = "") {
+    return allExit(Allex) + "\t" +
+        Code + "\t" +
+        Destination + "\t" +
+        IBIS1 + "\t" +
+        Top + "\t" +
+        Bottom + "\t" +
+        Side + "\t" +
+        "\t" +
+        RouteNum +
+        "\t\t" +
+        ImgPath + "\t" +
+        RearSmallTop + "\t" +
+        RearSmallBottom + "\t" +
+        "\t" +
+        RearBigTop + "\t" +
+        RearBigBottom + "\r\n";
+}
+
+function copyToClipboard(){
+    document.getElementById("output").select();
+    document.execCommand("copy");
+}
